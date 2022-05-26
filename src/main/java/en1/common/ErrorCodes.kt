1e05d8496df907898ccd224bbac54faa28ec56e9
audiz <lu41lbmcenuu@mail.ru>
2022-05-26 12:15:23 +0500

    rebuild in prod

--- src/main/java/en1/common/ErrorCodes.kt
+++ src/main/java/en1/common/ErrorCodes.kt
@@ -1,5 +1,7 @@
 package en1.common
-
+/**
+ * Error codes for app
+ * */
 const val ERROR_YANDEX_REQUEST_FAILED: Int = 1012
 const val ERROR_UNKNOWN_CALLBACK: Int = 1013
 const val ERROR_MSG_IN_YANDEX_JSON: Int = 1014
